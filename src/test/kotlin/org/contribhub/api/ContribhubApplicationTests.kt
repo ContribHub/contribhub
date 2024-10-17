aa5e1861691d974af6cc690fd118359175014447
rlfalsgh95 <rlfalsgh95@naver.com>
2024-10-17 22:16:41 +0900

    [TOY-3] initial commit

---
+++ src/test/kotlin/org/contribhub/api/ContribhubApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.contribhub.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ContribhubApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
