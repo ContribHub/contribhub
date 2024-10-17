8dde0239198b4534ad7b2881b756c97f7b0d2c1d
rlfalsgh95 <rlfalsgh95@naver.com>
2024-10-17 23:52:21 +0900

    [TOY-14] GithubClient 추가

---
+++ src/main/kotlin/org/contribhub/api/http/GithubClient.kt
@@ -0,0 +1,31 @@
+package org.contribhub.api.http
+
+import com.fasterxml.jackson.databind.JsonNode
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
+
+@Component
+class GithubClient(
+    private val webClient: WebClient,
+    @Value("\${api.github.auth-token}") private val apiToken: String,
+) {
+    private val host = "https://api.github.com"
+
+    suspend fun searchRepositories(
+        query: String,
+        sort: String? = "stars",
+        order: String? = "desc",
+        perPage: Int? = 10,
+        page: Int? = 1,
+    ): JsonNode =
+        webClient
+            .get()
+            .uri("$host/search/repositories?q=$query?sort=$sort&order=$order&per_page=$perPage&page=$page")
+            .headers {
+                it.add(HttpHeaders.AUTHORIZATION, "Bearer $apiToken")
+            }.retrieve()
+            .awaitBody()
+}
