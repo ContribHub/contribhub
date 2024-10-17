3fe0c3d0f0cd9c47b6e50dbdb0bd405da799f886
ethan-mino <rlfalsgh95@naver.com>
2024-10-17 22:51:57 +0900

    [TOY-3] initialize project (#1)

---
+++ src/main/kotlin/org/contribhub/api/controller/HomeController.kt
@@ -0,0 +1,10 @@
+package org.contribhub.api.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HomeController {
+    @GetMapping("/")
+    fun home(): String = "Hello, World!"
+}
