57ed510918cfa362160caac4e430cd00ed768fb1
rlfalsgh95 <rlfalsgh95@naver.com>
2024-10-25 18:53:54 +0900

    [TOY-5] 테스트 실패 복구

--- src/test/kotlin/org/contribhub/api/ContribhubApplicationTests.kt
+++ src/test/kotlin/org/contribhub/api/ContribhubApplicationTests.kt
@@ -2,12 +2,10 @@ package org.contribhub.api
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import kotlin.test.assertTrue
 
 @SpringBootTest
 class ContribhubApplicationTests {
     @Test
     fun contextLoads() {
-        assertTrue(false)
     }
 }
