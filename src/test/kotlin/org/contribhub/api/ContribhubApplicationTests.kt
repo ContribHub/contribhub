36edae4d8fa736749bab587428d1ac78949c280d
rlfalsgh95 <rlfalsgh95@naver.com>
2024-10-25 18:52:08 +0900

    [TOY-5] 테스트 실패

--- src/test/kotlin/org/contribhub/api/ContribhubApplicationTests.kt
+++ src/test/kotlin/org/contribhub/api/ContribhubApplicationTests.kt
@@ -2,10 +2,12 @@ package org.contribhub.api
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.assertTrue
 
 @SpringBootTest
 class ContribhubApplicationTests {
     @Test
     fun contextLoads() {
+        assertTrue(false)
     }
 }
