29509519e1c1a1f119f0a5450d21910b5852b6a7
rlfalsgh95 <rlfalsgh95@naver.com>
2024-11-04 00:02:32 +0900

    [TOY-29] Cron 설정

--- src/main/kotlin/org/contribhub/api/service/GithubCronService.kt
+++ src/main/kotlin/org/contribhub/api/service/GithubCronService.kt
@@ -1,9 +1,15 @@
 package org.contribhub.api.service
 
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
+
+@Service
 class GithubCronService(
     private val githubService: GithubService,
 ) {
-    // TODO 하루에 한번 수행하는 Cron 등록
+    // 새벽 1시에 수행하는 Cron Job
+    // TODO Replica 추가 시 쿠버네티스 CronJob으로 변경 가능
+    @Scheduled(cron = "0 */1 * * * ?")
     suspend fun fetchAndUpsertRepositoriesByTopics() {
         val topics = githubService.getTopics()
         val repositories = githubService.fetchLatestRepositoriesByTopics(topics)
