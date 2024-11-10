7890528372515e54984b5f14eb8e8056dc0d7a8c
rlfalsgh95 <rlfalsgh95@naver.com>
2024-11-10 12:33:23 +0900

    [TOY-30] loginPage GetMapping으로 변경

--- src/main/kotlin/org/contribhub/api/controller/ViewController.kt
+++ src/main/kotlin/org/contribhub/api/controller/ViewController.kt
@@ -3,13 +3,14 @@ package org.contribhub.api.controller
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.GetMapping
 
+@Deprecated("View Controller is will removed after front-end implementation")
 @Controller
 class ViewController(
     @Value("\${api.github.client-id}") private val clientId: String,
 ) {
-    @RequestMapping("/login")
+    @GetMapping("/login")
     fun loginPage(model: Model): String {
         model.addAttribute("clientId", clientId)
         return "loginView"
