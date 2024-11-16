9634e12740b261b95b07ab6ea86f6ee7c55eb513
lsh9672 <lsh80165@gmail.com>
2024-11-16 14:36:42 +0900

    [TOY-24] 테스트 에러 해소를 위해, 설정 변경

--- src/test/kotlin/org/contribhub/api/ContribhubApplicationTests.kt
+++ src/test/kotlin/org/contribhub/api/ContribhubApplicationTests.kt
@@ -2,8 +2,12 @@ package org.contribhub.api
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(locations = ["classpath:application-test.yml"])
 class ContribhubApplicationTests {
     @Test
     fun contextLoads() {
--- src/test/kotlin/org/contribhub/api/domain/opensourcerepository/repository/repositories/CustomRepositoryEntityRepositoryImplTest.kt
+++ src/test/kotlin/org/contribhub/api/domain/opensourcerepository/repository/repositories/CustomRepositoryEntityRepositoryImplTest.kt
@@ -1,19 +1,19 @@
 package org.contribhub.api.domain.opensourcerepository.repository.repositories
 
-import org.contribhub.api.infra.repository.entity.RepositoryEntity
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.springframework.beans.factory.annotation.Autowired
-import java.time.Instant
+// import org.contribhub.api.infra.repository.entity.RepositoryEntity
+// import org.junit.jupiter.api.Assertions.assertEquals
+// import org.springframework.beans.factory.annotation.Autowired
+// import java.time.Instant
 
 // TODO : 메모리 디비 설정을 안해서 테스트 시, 디비가 필요한 테스트 불가. 디비접근 설정 공유를 위해 테스트 주석처리
 // @SpringBootTest
 // @ActiveProfiles("test")
 class CustomRepositoryEntityRepositoryImplTest(
-    @Autowired private val repositoryEntityRepository: RepositoryEntityRepository,
+//    @Autowired private val repositoryEntityRepository: RepositoryEntityRepository,
 ) {
-//    @Test
-//    @DisplayName("레포지토리 upsert 기본 기능 테스트 - insert")
-//    @Rollback(true)
+    /*@Test
+    @DisplayName("레포지토리 upsert 기본 기능 테스트 - insert")
+    @Rollback(true)
     fun basicUpsertTest() {
         // given
         var repoList: MutableList<RepositoryEntity> = mutableListOf()
@@ -47,5 +47,5 @@ class CustomRepositoryEntityRepositoryImplTest(
         assertEquals(repositoryEntityRepository.findAll().size, 3)
 
         println(repositoryEntityRepository.findAll())
-    }
+    }*/
 }
