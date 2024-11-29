fb0d099c70a6238883a82257cd24db7a4214eb7d
lsh9672 <lsh80165@gmail.com>
2024-11-30 00:44:45 +0900

    [TOY-24] upsert 코드 변경 -> merge into 구문 사용

--- src/main/kotlin/org/contribhub/api/domain/opensourcerepository/repository/repositories/CustomRepositoryEntityRepositoryImpl.kt
+++ src/main/kotlin/org/contribhub/api/domain/opensourcerepository/repository/repositories/CustomRepositoryEntityRepositoryImpl.kt
@@ -18,53 +18,69 @@ class CustomRepositoryEntityRepositoryImpl(
             val query =
                 entityManager.createNativeQuery(
                     """
-                    INSERT INTO REPOSITORIES (
-                        repo_id, 
-                        repo_name, 
-                        repo_full_name, 
-                        repo_description, 
-                        owner_Name,
-                        owner_id,
-                        fork_count,
-                        open_issue_count,
-                        main_url,
-                        star_count,
-                        updated_dt)
-                    VALUES (
-                        :repoId,
-                        :repoName,
-                        :repoFullName,
-                        :repoDescription,
-                        :ownerName,
-                        :ownerId,
-                        :forkCount,
-                        :openIssueCount,
-                        :mainUrl,
-                        :starCount,
-                        now()
-                    )
-                    on conflict(repo_id)
-                    DO UPDATE SET
-                        repo_id = EXCLUDED.repo_id, 
-                        repo_name = EXCLUDED.repo_name, 
-                        repo_full_name = EXCLUDED.repo_full_name, 
-                        repo_description = EXCLUDED.repo_description, 
-                        owner_Name = EXCLUDED.owner_Name,
-                        owner_id = EXCLUDED.owner_id,
-                        fork_count = EXCLUDED.fork_count,
-                        open_issue_count = EXCLUDED.open_issue_count,
-                        main_url = EXCLUDED.main_url,
-                        star_count = EXCLUDED.star_count,
-                        updated_dt = EXCLUDED.updated_dt
+                    MERGE INTO REPOSITORIES
+                    USING(
+                        SELECT
+                            :repoId as repo_id,
+                            :repoName as repo_name,
+                            :repoFullName as repo_full_name,
+                            :repoDescription as repo_description,
+                            :ownerName as owner_name,
+                            :ownerId as owner_id,
+                            :forkCount as fork_count,
+                            :openIssueCount as open_issue_count,
+                            :mainUrl as main_url,
+                            :starCount as star_count
+                    ) as input_source
+                    ON REPOSITORIES.repo_id = input_source.repo_id
+                    WHEN MATCHED THEN
+                        UPDATE SET
+                            repo_id = input_source.repo_id, 
+                            repo_name = input_source.repo_name, 
+                            repo_full_name = input_source.repo_full_name, 
+                            repo_description = input_source.repo_description, 
+                            owner_Name = input_source.owner_Name,
+                            owner_id = input_source.owner_id,
+                            fork_count = input_source.fork_count,
+                            open_issue_count = input_source.open_issue_count,
+                            main_url = input_source.main_url,
+                            star_count = input_source.star_count,
+                            updated_dt = now()
+                    WHEN NOT MATCHED THEN
+                        INSERT (
+                            repo_id, 
+                            repo_name, 
+                            repo_full_name, 
+                            repo_description, 
+                            owner_Name,
+                            owner_id,
+                            fork_count,
+                            open_issue_count,
+                            main_url,
+                            star_count,
+                            updated_dt
+                        ) VALUES (
+                            input_source.repo_id,
+                            input_source.repo_name,
+                            input_source.repo_full_name,
+                            input_source.repo_description,
+                            input_source.owner_name,
+                            input_source.owner_id,
+                            input_source.fork_count,
+                            input_source.open_issue_count,
+                            input_source.main_url,
+                            input_source.star_count,
+                            now()
+                        )
                     """.trimIndent(),
                 )
 
-            query.setParameter("repoId", data.repoId)
+            query.setParameter("repoId", data.repoId.toString())
             query.setParameter("repoName", data.repoName)
             query.setParameter("repoFullName", data.repoFullName)
             query.setParameter("repoDescription", data.repoDescription)
             query.setParameter("ownerName", data.ownerName)
-            query.setParameter("ownerId", data.ownerId)
+            query.setParameter("ownerId", data.ownerId.toString())
             query.setParameter("forkCount", data.forkCount)
             query.setParameter("openIssueCount", data.openIssueCount)
             query.setParameter("mainUrl", data.mainUrl)
