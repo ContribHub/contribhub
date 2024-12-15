ad373dbdd840adee22d4bf3c39edf826bb6b2ac6
lsh9672 <lsh80165@gmail.com>
2024-12-15 15:57:16 +0900

    [TOY-40] 특정 토픽에 해당하는 레포지토리 seq 목록반환 기능 추가(일대다 관계로 쿼리 분리해서 처리.)

--- src/main/kotlin/org/contribhub/api/domain/opensourcerepository/repository/mapping/CustomTopicRepositoryEntityRepositoryImpl.kt
+++ src/main/kotlin/org/contribhub/api/domain/opensourcerepository/repository/mapping/CustomTopicRepositoryEntityRepositoryImpl.kt
@@ -1,7 +1,26 @@
 package org.contribhub.api.domain.opensourcerepository.repository.mapping
 
 import com.linecorp.kotlinjdsl.support.spring.data.jpa.repository.KotlinJdslJpqlExecutor
+import org.contribhub.api.domain.opensourcerepository.entity.TopicRepositoryEntity
+import org.contribhub.api.domain.opensourcerepository.entity.TopicRepositoryId
 
 class CustomTopicRepositoryEntityRepositoryImpl(
     private val jpqlExecutor: KotlinJdslJpqlExecutor,
-) : CustomTopicRepositoryEntityRepository
+) : CustomTopicRepositoryEntityRepository {
+    override fun findRepositoryListInTopic(topicId: Long?): List<Long> {
+        // 검색어로 들어온 topicId가 null이면 빈 리스트 반환.
+        return topicId?.let {
+            jpqlExecutor.findAll {
+                selectNew<Long>(
+                    path(TopicRepositoryEntity::topicRepositoryId).path(TopicRepositoryId::repoSeq)
+                        .`as`(expression(Long::class, "repoSeq")),
+                ).from(
+                    entity(TopicRepositoryEntity::class),
+                ).whereAnd(
+                    path(TopicRepositoryEntity::useYn).eq(true),
+                    path(TopicRepositoryEntity::topicRepositoryId).path(TopicRepositoryId::topicSeq).eq(topicId),
+                )
+            }.filterNotNull()
+        } ?: emptyList()
+    }
+}
