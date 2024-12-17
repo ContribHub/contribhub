48ed312b5ef20148ba8ac36cef2bbe7ba1562520
rlfalsgh95 <rlfalsgh95@naver.com>
2024-12-17 23:38:40 +0900

    [TOY-44] buildSrc 모듈 추가

---
+++ buildSrc/src/main/kotlin/org/contribhub/Versions.kt
@@ -0,0 +1,9 @@
+package org.contribhub
+
+object Versions {
+    const val COROUTINE_VERSION = "1.8.1"
+    const val JACKSON_KOTLIN = "2.17.0"
+    const val KOTLIN_JSDL = "3.5.3"
+    const val TEST_CONTAINER = "1.19.1"
+    const val LOGGER = "7.0.0"
+}
--- infra/build.gradle.kts
+++ infra/build.gradle.kts
@@ -1,10 +1,4 @@
-object Versions {
-    const val COROUTINE_VERSION = "1.8.1"
-    const val JACKSON_KOTLIN = "2.17.0"
-    const val KOTLIN_JSDL = "3.5.3"
-    const val TEST_CONTAINER = "1.19.1"
-    const val LOGGER = "7.0.0"
-}
+import org.contribhub.Versions
 
 plugins {
     kotlin("plugin.spring") version "1.9.25"
