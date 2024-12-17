b276c7da39b7629c6b56011fc775b8a102c150a7
rlfalsgh95 <rlfalsgh95@naver.com>
2024-12-17 23:58:49 +0900

    [TOY-44] 의존성 정리

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -18,7 +18,6 @@ dependencies {
     implementation("io.github.oshai:kotlin-logging-jvm:${Versions.LOGGER}")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.boot:spring-boot-starter-security")
 }
 
